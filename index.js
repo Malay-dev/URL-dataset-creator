2f20595cbf62018f191f0a4e40935c83d25db7c9
Malay Kumar <malaykumar2003@gmail.com>
2023-11-28 20:47:27 +0530

    Feat: database init

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import express from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
+
+import { connect_to_rabbit_mq } from "./utility/message_queue.js";
 
 dotenv.config();
 
@@ -22,6 +25,22 @@ app.enable("trust proxy");
 app.use(url_logger(true));
 app.use(express.json());
 app.use(express.static("public"));
+
+const MONGO_USER = process.env.MONGO_USER;
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+const DATABASE_URL = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@url-dataset-cluster.kfsuavg.mongodb.net/?retryWrites=true&w=majority`;
+
+const connect_to_database = () => {
+  mongoose
+    .connect(DATABASE_URL)
+    .then(() => console.log("[server]: Connected to database..."))
+    .catch((e) => {
+      console.log(`[server]: ${e}`);
+      setTimeout(connect_to_database, 5000);
+    });
+};
+connect_to_database();
+connect_to_rabbit_mq();
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
